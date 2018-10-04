d6835853888f5ba20845134297429c92a9093cd7
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-10-04 14:46:47 -0600

    test DELET /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -109,3 +109,44 @@ describe('GET /todos/:id', () => {
       .end(done)
   })
 })
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', done => {
+    const hexId = todos[1]._id.toHexString()
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(hexId)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        Todo.findById(hexId)
+          .then(todo => {
+            expect(todo).toNotExist()
+            done()
+          })
+          .catch(e => done(e))
+      })
+  })
+
+  it('should return 404 if todo not found', done => {
+    const hexId = new ObjectID().toHexString()
+
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done)
+  })
+
+  it('should return 404 if object id is invalid', done => {
+    request(app)
+      .get('/todos/abc123')
+      .expect(404)
+      .end(done)
+  })
+})
