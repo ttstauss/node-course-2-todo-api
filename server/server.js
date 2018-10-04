0e0a60b6945cc0dbb182cd503e918c1b9ad7447a
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-10-04 13:31:29 -0600

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const { ObjectID } = require('mongodb')
 
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
@@ -33,6 +34,28 @@ app.get('/todos', (req, res) => {
       res.status(400).send(e)
     }
   )
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id)
+    .then(
+      todo => {
+        if (!todo) {
+          return res.status(404).send()
+        }
+        res.send({ todo })
+      },
+      e => {
+        return res.status(400).send()
+      }
+    )
+    .catch(e => res.status(400).send())
 })
 
 app.listen(3000, () => {
