07773e7c4743c419598ad2c76d9dcc43d51e66a7
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-10-05 12:38:12 -0600

    add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -214,11 +214,13 @@ describe('POST /users', () => {
           return done(err)
         }
 
-        User.findOne({ email }).then(user => {
-          expect(user).toExist()
-          expect(user.password).toNotBe(password)
-          done()
-        })
+        User.findOne({ email })
+          .then(user => {
+            expect(user).toExist()
+            expect(user.password).toNotBe(password)
+            done()
+          })
+          .catch(e => done(e))
       })
   })
 
@@ -236,5 +238,54 @@ describe('POST /users', () => {
       .send({ email: users[0].email, password: 'abc123!' })
       .expect(400)
       .end(done)
+  })
+})
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', done => {
+    request(app)
+      .post('/users/login')
+      .send({ email: users[1].email, password: users[1].password })
+      .expect(200)
+      .expect(res => {
+        expect(res.header['x-auth']).toExist()
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        User.findById(users[1]._id)
+          .then(user => {
+            expect(user.tokens[0]).toInclude({
+              access: 'auth',
+              token: res.headers['x-auth']
+            })
+            done()
+          })
+          .catch(e => done(e))
+      })
+  })
+
+  it('should reject invalid login', done => {
+    request(app)
+      .post('/users/login')
+      .send({ email: users[1].email, password: users[1].password + '123' })
+      .expect(400)
+      .expect(res => {
+        expect(res.header['x-auth']).toNotExist()
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        User.findById(users[1]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0)
+            done()
+          })
+          .catch(e => done(e))
+      })
   })
 })
