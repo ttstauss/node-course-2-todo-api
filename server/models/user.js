934efa175de07675c30cdcb7d3a6c45a7a488ed0
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-10-05 13:52:58 -0600

    update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -46,7 +46,7 @@ UserSchema.methods.generateAuthToken = function() {
   const user = this
   const access = 'auth'
   const token = jwt
-    .sign({ _id: user._id.toHexString(), access }, 'abc123')
+    .sign({ _id: user._id.toHexString(), access }, process.env.JWT_SECRET)
     .toString()
 
   user.tokens.push({ access, token })
@@ -70,7 +70,7 @@ UserSchema.statics.findByToken = function(token) {
   let decoded
 
   try {
-    decoded = jwt.verify(token, 'abc123')
+    decoded = jwt.verify(token, process.env.JWT_SECRET)
   } catch (e) {
     return Promise.reject()
   }
