3bafb660c6f40b996eb840b421d17f207d13ba9d
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-10-05 12:46:35 -0600

    add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,16 @@ UserSchema.methods.generateAuthToken = function() {
   })
 }
 
+UserSchema.methods.removeToken = function(token) {
+  const user = this
+
+  return user.update({
+    $pull: {
+      tokens: { token }
+    }
+  })
+}
+
 UserSchema.statics.findByToken = function(token) {
   const User = this
   let decoded
