22410e66733c508130755a045776b4c557712de9
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-10-05 12:07:07 -0600

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -72,6 +72,22 @@ UserSchema.statics.findByToken = function(token) {
   })
 }
 
+UserSchema.statics.findByCredentials = function(email, password) {
+  const User = this
+
+  return User.findOne({ email }).then(user => {
+    if (!user) {
+      return Promise.reject()
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        res ? resolve(user) : reject()
+      })
+    })
+  })
+}
+
 UserSchema.pre('save', function(next) {
   const user = this
 
